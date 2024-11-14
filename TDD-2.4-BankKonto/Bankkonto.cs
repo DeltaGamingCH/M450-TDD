6f4af4a03fb18479a8aade2ed82fb5ebb4355944
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-11-14 17:12:10 +0100

    feat(interest): implemented SchreibeZinsGut(), calculating active and passive interest depending on balance

--- TDD-2.4-BankKonto/Bankkonto.cs
+++ TDD-2.4-BankKonto/Bankkonto.cs
@@ -12,10 +12,13 @@ namespace TDD_2._4_BankKonto
         public double Guthaben { get; private set; }
         public double aktivZins = 0.25;
         public double passivZins = 0.5;
+        //int anzahlTage = 360;
 
-        public Bankkonto(int kontoNummer)
+        public Bankkonto(int kontoNummer, double guthaben)
         { 
             KontoNummer = kontoNummer;
+
+            Guthaben = guthaben;
 
             /*
             if (kontoNummer != null && kontoNummer != 0)
@@ -23,8 +26,6 @@ namespace TDD_2._4_BankKonto
                 Random random = new Random();
                 kontoNummer = random.Next(10000000, 99999999);
             }*/
-
-            Guthaben = 0;
         }
 
         public void ZahleEin(double betrag)
@@ -45,9 +46,23 @@ namespace TDD_2._4_BankKonto
 
         }
 
-        public void SchreibeZinsGut(int anzahlTage)
+        public double SchreibeZinsGut(int anzahlTage)
         {
+            double zinsBetrag = 0;
 
+            for (int i = 0; i < anzahlTage; i++)
+            {
+                if (Guthaben >= 0)
+                {
+                    zinsBetrag = zinsBetrag + (Guthaben * aktivZins / 100);
+                }
+                else
+                {
+                    zinsBetrag = zinsBetrag + (Guthaben * passivZins / 100);
+                }
+            }
+
+            return zinsBetrag;
         }
 
         public void SchlisseKontoAb()
